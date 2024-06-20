4fbe2296d6817e09eb045e9672003609d61ebe7b
jognhoon <jognhoon7431@gmail.com>
2024-06-20 17:25:50 +0900

    chore: 병합

--- src/components/detailpage/readPost/ReadPost.jsx
+++ src/components/detailpage/readPost/ReadPost.jsx
@@ -21,11 +21,15 @@ import {
   StTitleSection
 } from './readPost.styled';
 
-const ReadPost = ({ setIsEdit, userInfo }) => {
+const ReadPost = ({ setIsEdit, targetData, userInfo }) => {
   const navigate = useNavigate();
   const { id: postId } = useParams();
   const queryClient = useQueryClient();
   const [commentIsEdit, setCommentIsEdit] = useState(false);
+
+  localStorage.setItem('address', targetData.address);
+  localStorage.setItem('x', targetData.coordinate?.lng);
+  localStorage.setItem('y', targetData.coordinate?.lat);
 
   //포스트 정보 가져오기
   const {
